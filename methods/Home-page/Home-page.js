f8db4b9aa707900676c7890bd7bf39d943c09e84
andres2503r <rios73715@gmail.com>
2022-09-23 15:53:37 -0500

    change page device, home, input, lan, ouput, wiegand

--- methods/Home-page/Home-page.js
+++ methods/Home-page/Home-page.js
@@ -13,3 +13,16 @@ btn.forEach((value, i) =>{
         
     })
 })
+// buttons method
+const table = document.querySelectorAll(".table")
+const button = document.querySelectorAll(".btnfunction")
+
+button.forEach((cadaButton, i)=>{
+    button[i].addEventListener(`click`, ()=>{
+        table.forEach((cadaTable, i)=>{
+            table[i].classList.add(`tablefunction`)
+            
+        })
+        table[i].classList.remove(`tablefunction`)
+    })
+})
